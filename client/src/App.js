12b7b9ad6e5f2b4013c927cb5e0273ea13d08c99
MaxwellChrist <ribthulu@gmail.com>
2022-02-21 16:36:18 -0500

    got API call working with MovieList.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ export default function App (props) {
       axios
         .get('http://localhost:5001/api/movies') // Study this endpoint with Postman
         .then(response => {
-          console.log(response.data);
+          // console.log(response.data);
           setMovieList(response.data);
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
@@ -31,13 +31,11 @@ export default function App (props) {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
-  console.log(MovieList)
-
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
         <Route path="/"> 
-          <MovieList movie={movieList}/>
+          <MovieList movies={movieList}/>
         </Route>
         <Route path={"/movies/:movieID"}> 
           <Movie />
